4a2947a0cf8a44050d38e7ea643ec696246e68ec
Karan Sinha <76760451+karansinha12@users.noreply.github.com>
2021-10-09 17:40:46 +0530

    Create inorder.java

---
+++ inorder.java
@@ -0,0 +1,13 @@
+class Inorder{
+
+    void printInorder(Node node)
+    {
+        if (node == null)
+            return;
+        printInorder(node.left);
+        System.out.print(node.key + " ");
+        printInorder(node.right);
+    }
+
+    
+}
